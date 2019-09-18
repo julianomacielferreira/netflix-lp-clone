7e2fda04284650e222b8595654ac674611922090
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-09-18 05:29:27 -0300

    Implement javascript to handle tabs functionality.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log('hello world');
+// Get the list of all tab item and content item elements
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// Select tab content item
+function selectItem(e) {
+
+    // Remove the border from all tab items
+    removeBorder();
+
+    // Remove the show from all tab content items
+    removeShow();
+
+    // Add border to current tab
+    this.classList.add("tab-border");
+
+    // Grab content item from the DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+    tabItems.forEach(item => {
+        item.classList.remove("tab-border");
+    });
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => {
+        item.classList.remove("show");
+    });
+}
+
+// Listen for tab click
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItem);
+});
